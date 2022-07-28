56f6cac93273f6167a93c287cad9337be4d1f6f6
Nicolas Bueno <nicolasbrian202@gmail.com>
2022-07-28 11:55:47 -0300

    execicio 4

---
+++ Exercicios/exer04.js
@@ -0,0 +1,7 @@
+// Escreva uma funçao chamada ePaisagem que recebe
+// dois argumentos, largura e altura de uma imagem
+// e retorne true se a imagem estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
