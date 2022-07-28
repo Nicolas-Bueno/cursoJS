cf67df3efdff71b602813d2bf2313cffaa7ea101
Nicolas Bueno <nicolasbrian202@gmail.com>
2022-07-28 11:52:48 -0300

    exercicio 3

---
+++ Exercicios/exer03.js
@@ -0,0 +1,11 @@
+// Escreva uma funcao que recebe 2 numeros e retorne o maior deles
+
+function verificaMaior(x, y) {
+   if (x > y) return x;
+    return y
+   
+}
+
+const maior = verificaMaior(50, 62);
+
+console.log(maior);
